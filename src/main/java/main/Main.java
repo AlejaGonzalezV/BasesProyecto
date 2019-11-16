5543bbd0d603bea0413a3afd4829d96abf4e5dc7
AlejaGonzalezV <aleja.gonzalezv19@gmail.com>
2019-11-15 19:06:48 -0500

    Conexion establecida

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application{
 
 	@Override
@@ -22,7 +24,36 @@ public class Main extends Application{
 	}
 	
 	public static void main (String[] args) {
-		launch(args);
+		Connection conexion = null;
+		String jdbcUni = "jdbc:oracle:thin:@172.16.0.103:1522/ESTUD";
+		String jdbcCasa = "jdbc:oracle:thin:@200.3.193.24:1522/ESTUD";
+
+		
+		try {
+
+			conexion = DriverManager.getConnection(jdbcUni, "P09551_1_18", "1007554028");
+//			conexion = DriverManager.getConnection(jdbcCasa, "P09551_1_18", "1007554028");
+
+			System.out.println("Conectado");
+			launch(args);
+			
+		} catch(SQLException sql) {
+			
+			sql.printStackTrace();
+		} finally {
+			if(conexion != null) {
+				
+				try {
+					conexion.close();
+					System.out.println("Desconectado");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+				
+			}
+			
+		}
 		
 		
 		
